3c5554293c3b36ea1ed335392063d9175498d948
Satria Bagus <satria_bagus@hotmail.com>
2017-05-24 20:33:10 +0700

    init laravel

---
+++ app/Http/Controllers/BackendController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+
+class BackendController extends Controller
+{
+    //
+    public function test(){
+        $client = new Client();
+        $res = $client->request('POST', 'https://api.bukalapak.com/v2/authenticate.json', [
+            'auth' => ['', '']
+        ]);
+        $response = json_decode($res->getBody(), true);
+        if($response['status'] == 'OK') {
+            $result = array("id" => $response['user_id'], "user_name" => $response['user_name'],
+                "email" => $response['email'], "token" => $response['token']);
+            return json_encode($result);
+        } else {
+            return $response;
+        }
+
+    }
+}
