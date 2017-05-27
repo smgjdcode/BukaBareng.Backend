6d9e68ac5a6d0263e081146ea72d2ef6539755ae
Satria Bagus <satria_bagus@hotmail.com>
2017-05-28 00:38:33 +0700

    finished

---
+++ database/migrations/Undefined index: 2017_05_25_141117_create_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('jumlah');
+            $table->string('product_id')->unique();
+            $table->integer('massdrop_id')->unsigned();
+            $table->string('user_id')->unsigned();
+            $table->integer('status');
+            $table->date('bought_time');
+            $table->timestamps();
+            $table->foreign('massdrop_id')->references('id')->on('massdrops');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
